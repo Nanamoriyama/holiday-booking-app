3629d61ca630c692d657dfb3dc170b533ddab608
Nanamoriyama <nanamoriyama@outlook.jp>
2024-05-01 00:21:39 +0200

    added components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
 import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
@@ -26,10 +27,10 @@ const App = () => {
           }
         />
         <Route
-          path="search"
+          path="/search"
           element={
             <Layout>
-              <p>Search Page</p>
+              <Search />
             </Layout>
           }
         />
