ee35f47debcdbcf9ff16792f01410cb0a77f91e2
Nanamoriyama <nanamoriyama@outlook.jp>
2024-05-01 17:21:50 +0200

    final commit added home page

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,13 +2,16 @@ const Footer = () => {
   return (
     <div className="bg-blue-800 py-10">
       <div className="container mx-auto flex justify-between items-center">
-        <span className="text-3xl text-white font-bold tracking-tight"></span>
+        <span className="text-3xl text-white font-bold tracking-tight">
+          MernHolidays.com
+        </span>
         <span className="text-white font-bold tracking-tight flex gap-4">
-          <p className="curso-pointer">Privacy Policy</p>
-          <p className="curso-pointer">Terms of Servise</p>
+          <p className="cursor-pointer">Privacy Policy</p>
+          <p className="cursor-pointer">Terms of Service</p>
         </span>
       </div>
     </div>
   );
 };
+
 export default Footer;
